e4202718aade5f31e34260ce166fc069583fb943
A-kun <maimai53kantai@gmail.com>
2025-05-12 07:40:58 +0900

    pubspecなどをexampleそのままになってた状態から修正、実機テスト完了

--- android/app/src/main/kotlin/com/example/uaal_flutter_324/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/uaal_flutter_324/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.uaal_flutter_324
 
 import io.flutter.embedding.android.FlutterActivity
+import com.xraph.plugin.flutter_unity_widget.FlutterUnityActivity;
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterUnityActivity() 
